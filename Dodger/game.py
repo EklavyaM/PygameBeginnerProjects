5334021e9e81295601c58b7a08507c13cb26f7c4
eklavya <klad.spear@gmail.com>
2018-09-01 17:17:28 +0530

    Changed player movement and added upward direction spawner as well.

--- Dodger/game.py
+++ Dodger/game.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 from random import randint
-from threading import Thread
+from threading import Timer
 from time import sleep
 
 
@@ -13,8 +13,8 @@ class AssholeSpawner:
     # ==================== The THRESHOLD_DIFFICULTY value is the minimum sleep time required =====================
     # ==================== by the Spawning thread. Any less than 0.07 makes the game far too difficult =============
 
-    DIFFICULTY_VALUE = 0.07
-    THRESHOLD_DIFFICULTY = 0.07
+    DIFFICULTY_VALUE = 0.04
+    THRESHOLD_DIFFICULTY = 0.04
 
     def __init__(self, l_player):
 
@@ -22,12 +22,14 @@ class AssholeSpawner:
         # ==================== Needs a player instance for collision detection =====================================
 
         self.__assholes = []
-        self.__thread_spawner = Thread(target=self.__spawn)
+        self.__thread_spawner = Timer(3, self.__spawn)
         self.__sleep_time = 0.8
         self.__temp_pos_x = 0
+        self.__temp_pos_y = 0
         self.__temp_vel = 0
         self.__temp_size_x = 0
         self.__temp_size_y = 0
+        self.__temp_type_index = 0
 
         self.__player = l_player
 
@@ -44,11 +46,23 @@ class AssholeSpawner:
         # ==================== Change values for different asshole behavior and visuals ============================
 
         while g_game_running:
-            self.__temp_pos_x = randint(0, g_game_area_dimensions[0])
+
             self.__temp_vel = randint(10, 700)
             self.__temp_size_x = self.__temp_size_y = randint(8, 20)
-            self.__assholes.append(AssholeSpawner.Asshole(self.__temp_pos_x, -self.__temp_size_y, self.__temp_vel,
-                                                          self.__temp_size_x, self.__temp_size_y))
+            self.__temp_type_index = randint(0, len(AssholeSpawner.Asshole.TYPES) - 1)
+
+            self.__temp_pos_x = randint(0, g_screen_dimensions[0] - self.__temp_size_x)
+
+            if AssholeSpawner.Asshole.TYPES[self.__temp_type_index] == AssholeSpawner.Asshole.DIR_DOWN:
+                self.__temp_pos_y = -self.__temp_size_y
+
+            elif AssholeSpawner.Asshole.TYPES[self.__temp_type_index] == AssholeSpawner.Asshole.DIR_UP:
+                self.__temp_pos_y = g_screen_dimensions[1]
+
+            self.__assholes.append(AssholeSpawner.Asshole(self.__temp_pos_x, self.__temp_pos_y,
+                                                          self.__temp_vel,
+                                                          self.__temp_size_x, self.__temp_size_y,
+                                                          self.__temp_type_index))
             sleep(self.__sleep_time)
 
     def increase_difficulty(self):
@@ -75,9 +89,11 @@ class AssholeSpawner:
 
             if asshole.check_player_collision(self.__player):
                 global g_game_running
-                g_game_running = False
 
-                print("Score: " + get_score())
+                if not asshole.get_has_collided():
+
+                    asshole.set_has_collided(True)
+                    self.__player.hit()
 
     def draw(self, scr):
 
@@ -87,9 +103,17 @@ class AssholeSpawner:
             asshole.draw(scr)
 
     # ==================== Inner Class Asshole for Spawner to create instances of ==================================
+
     class Asshole:
 
-        def __init__(self, l_pos_x, l_pos_y, l_vel, l_size_x, l_size_y):
+        # ==================== Different Types of Assholes ========================================================
+
+        DIR_UP = 0
+        DIR_DOWN = 1
+
+        TYPES = (DIR_DOWN, DIR_UP)
+
+        def __init__(self, l_pos_x, l_pos_y, l_vel, l_size_x, l_size_y, l_type_index):
 
             # ==================== Asshole Constructor ==========================================================
 
@@ -99,6 +123,9 @@ class AssholeSpawner:
             self.__size_x = l_size_x
             self.__size_y = l_size_y
             self.__color = pygame.Color("red")
+
+            self.__type = AssholeSpawner.Asshole.TYPES[l_type_index]
+
             self.__is_destroyed = False
             self.__has_collided = False
             self.__hit_box = pygame.Rect(self.__pos_x, self.__pos_y, self.__size_x, self.__size_y)
@@ -109,9 +136,16 @@ class AssholeSpawner:
             # ==================== Along with its hit box =======================================================
 
             if not self.__has_collided:
-                self.__pos_y = self.__hit_box.top = self.__pos_y + self.__velocity * g_time_per_frame_in_seconds
+
+                if self.__type == AssholeSpawner.Asshole.DIR_DOWN:
+                    self.__pos_y = self.__hit_box.top = self.__pos_y + self.__velocity * g_time_per_frame_in_seconds
+
+                elif self.__type == AssholeSpawner.Asshole.DIR_UP:
+                    self.__pos_y = self.__hit_box.top = self.__pos_y - self.__velocity * g_time_per_frame_in_seconds
+
             else:
                 self.__color.r -= 10
+
                 if self.__color.r <= 10:
                     self.__is_destroyed = True
                     pass
@@ -145,11 +179,17 @@ class AssholeSpawner:
         def get_is_destroyed(self):
             return self.__is_destroyed
 
+        def boundary_check(self):
+
             # ==================== Check if Asshole crossed the Boundary to Assguard ================================
 
-        def boundary_check(self):
-            if self.__pos_y + self.__size_y > g_game_area_dimensions[1]:
-                self.__has_collided = True
+            if self.__type == AssholeSpawner.Asshole.DIR_DOWN:
+                if self.__pos_y + self.__size_y >= g_screen_dimensions[1] - g_font_screen_offset:
+                    self.__has_collided = True
+
+            elif self.__type == AssholeSpawner.Asshole.DIR_UP:
+                if self.__pos_y <= 0:
+                    self.__has_collided = True
 
 
 class Player:
@@ -158,10 +198,22 @@ class Player:
 
     SIZE_X = 16
     SIZE_Y = 16
-    ACC = 200
+
+    REG_ACC = 200
+    REG_COLOR = "white"
+
+    STUN_ACC = 50
+    STUN_COLOR = "blue"
+    STUN_TIME = 3.5
+
     MAX_VELOCITY = 500
     MIN_VELOCITY = 0.5
     FRICTION = 0.3
+
+    DIR_UP = 0
+    DIR_DOWN = 1
+    DIR_LEFT = 2
+    DIR_RIGHT = 3
 
     def __init__(self, l_pos_x, l_pos_y):
 
@@ -171,29 +223,41 @@ class Player:
         self.pos_y = l_pos_y
         self.vel_x = 0
         self.vel_y = 0
+        self.acc = Player.REG_ACC
+
         self.hit_box = pygame.Rect(self.pos_x, self.pos_y, Player.SIZE_X, Player.SIZE_Y)
-        self.color = pygame.Color("white")
+        self.color = pygame.Color(Player.REG_COLOR)
+        self.lives = 3
+        self.current_dir = None
+
+        self.keys = None
+        self.dx, self.dy = 0, 0
+
+        self.stun_timer = None
 
     def move(self):
 
         # ==================== Polling for Input =================================================================
 
-        keys = pygame.key.get_pressed()
-        dx, dy = 0, 0
+        self.keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            dx -= 1
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            dx += 1
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            dy -= 1
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            dy += 1
+        if self.dx != 1 and (self.keys[pygame.K_LEFT] or self.keys[pygame.K_a]):
+            self.dy = 0
+            self.dx = -1
+        if self.dx !=-1 and (self.keys[pygame.K_RIGHT] or self.keys[pygame.K_d]):
+            self.dy = 0
+            self.dx = 1
+        if self.dy != 1 and (self.keys[pygame.K_UP] or self.keys[pygame.K_w]):
+            self.dy = -1
+            self.dx = 0
+        if self.dy != -1 and (self.keys[pygame.K_DOWN] or self.keys[pygame.K_s]):
+            self.dy = 1
+            self.dx = 0
 
         # ==================== Accelerating Player till reaching Max Velocity =========================================
 
-        updated_vel_x = self.vel_x + dx * Player.ACC
-        updated_vel_y = self.vel_y + dy * Player.ACC
+        updated_vel_x = self.vel_x + self.dx * self.acc
+        updated_vel_y = self.vel_y + self.dy * self.acc
 
         if math.fabs(updated_vel_x) < Player.MAX_VELOCITY:
             self.vel_x = updated_vel_x
@@ -220,13 +284,13 @@ class Player:
         l_temp_pos_x = self.pos_x + self.vel_x * g_time_per_frame_in_seconds
         l_temp_pos_y = self.pos_y + self.vel_y * g_time_per_frame_in_seconds
 
-        if l_temp_pos_x > g_game_area_dimensions[0] - Player.SIZE_X:
-            l_temp_pos_x = g_game_area_dimensions[0] - Player.SIZE_X
+        if l_temp_pos_x > g_screen_dimensions[0] - Player.SIZE_X:
+            l_temp_pos_x = g_screen_dimensions[0] - Player.SIZE_X
         elif l_temp_pos_x < 0:
             l_temp_pos_x = 0
 
-        if l_temp_pos_y > g_game_area_dimensions[1] - Player.SIZE_Y:
-            l_temp_pos_y = g_game_area_dimensions[1] - Player.SIZE_Y
+        if l_temp_pos_y > g_screen_dimensions[1] - Player.SIZE_Y:
+            l_temp_pos_y = g_screen_dimensions[1] - Player.SIZE_Y
         elif l_temp_pos_y < 0:
             l_temp_pos_y = 0
 
@@ -241,6 +305,55 @@ class Player:
 
         pygame.draw.rect(scr, self.color, self.hit_box)
 
+    def hit(self):
+
+        # ==================== When Player is Hit =================================================================
+
+        self.lives -= 1
+
+        if self.lives <= 0:
+            self.kill()
+        else:
+            self.stun()
+
+    def stun(self):
+
+        # ==================== When Player is Stunned start a stun timer ============================================
+
+        self.acc = Player.STUN_ACC
+        self.color = pygame.Color(Player.STUN_COLOR)
+
+        if not self.stun_timer:
+            self.stun_timer = Timer(Player.STUN_TIME, self.un_stun)
+            self.stun_timer.start()
+
+        else:
+            if self.stun_timer.is_alive():
+                self.stun_timer.cancel()
+
+            self.stun_timer = Timer(Player.STUN_TIME, self.un_stun)
+            self.stun_timer.start()
+
+    def un_stun(self):
+
+        # ==================== Called when stun timer ends ===========================================================
+
+        self.acc = Player.REG_ACC
+        self.color = pygame.Color(Player.REG_COLOR)
+        print("unstunned")
+
+    def kill(self):
+
+        # ==================== When Player is Killed =================================================================
+
+        global g_game_running
+
+        if self.stun_timer:
+            self.stun_timer.cancel()
+
+        g_game_running = False
+        print("Score " + get_score())
+
 
 def render():
 
@@ -251,8 +364,12 @@ def render():
     g_screen.fill(pygame.Color("black"))
 
     g_score_text = g_font.render("Score: " + get_score(), True, (255, 255, 255))
+    g_lives_text = g_font.render("Lives: " + str(g_player.lives), True, (255, 255, 255))
+
     g_screen.blit(g_score_text,
                   (g_screen_dimensions[0]//2 - g_score_text.get_rect().width//2, g_screen_dimensions[1] - g_font_size))
+
+    g_screen.blit(g_lives_text, (g_font_screen_offset, g_screen_dimensions[1] - g_font_size))
 
     g_asshole_spawner.draw(g_screen)
     g_player.draw(g_screen)
@@ -283,7 +400,7 @@ def init():
     g_screen = pygame.display.set_mode(g_screen_dimensions)
     pygame.display.set_caption(g_screen_title)
 
-    g_player = Player(g_game_area_dimensions[0] // 2, g_game_area_dimensions[1] - Player.SIZE_Y)
+    g_player = Player(g_screen_dimensions[0] // 2, g_screen_dimensions[1] // 2)
     g_asshole_spawner = AssholeSpawner(g_player)
 
 
@@ -307,7 +424,7 @@ def check_score_and_increase_difficulty():
     global g_score_frame_increment, g_check_score_min_bound, g_asshole_spawner
 
     if g_check_score_min_bound < g_score < g_check_score_multiplier * g_check_score_min_bound:
-        g_score_frame_increment += 0.01
+        g_score_frame_increment += 0.02
 
     elif g_score > g_check_score_multiplier * g_check_score_min_bound:
         g_asshole_spawner.increase_difficulty()
@@ -349,7 +466,7 @@ if __name__ == "__main__":
 
     # ====================  Global Declaration  =================================================================
 
-    g_screen_dimensions = (1366, 768)
+    g_screen_dimensions = (1600, 900)
     g_screen_title = "Dodger"
     g_screen = None
 
@@ -369,8 +486,7 @@ if __name__ == "__main__":
     g_font_size = 32
     g_font_style = "comicsansms"
     g_score_text = None
-
-    g_game_area_dimensions = (g_screen_dimensions[0], g_screen_dimensions[1] - g_font_size)
+    g_font_screen_offset = 10
 
     # ====================  A Min Score for Dynamic Difficulty Increase ============================================
 
